fda8f04fe003f38a03cf9581389498d0620e860d
wahidalqorni <idunalqorni@gmail.com>
2024-05-11 15:06:55 +0700

    update

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -11,67 +11,72 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: bgColor,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // baris ke-1
-          Container(
-            margin: EdgeInsets.only(left: 20),
-            child: Row(
-              children: [
-                Image.asset("assets/image1.png"),
-                SizedBox(
-                  width: 9,
-                ),
-                Text(
-                  "Buburger",
-                  style: splashTextstyle1.copyWith(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: bgColor,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // baris ke-1
+            Container(
+              margin: EdgeInsets.only(left: 20, top: MediaQuery.of(context).size.height * 0.10),
+              child: Row(
+                children: [
+                  Image.asset("assets/image1.png"),
+                  SizedBox(
+                    width: 9,
                   ),
-                ),
-              ],
+                  Text(
+                    "Buburger",
+                    style: splashTextstyle1.copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          // baris ke-2
-          Container(
-            margin: EdgeInsets.only(left: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Jagonya",
-                  style: splashTextstyle2.copyWith(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                Text(
-                  "Burger!",
-                  style: splashTextstyle2.copyWith(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ],
+            SizedBox(
+              height: 20,
             ),
-          ),
-
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.20,
-          ),
-          // baris ke-3
-          Image.asset(
-            "assets/img-splash.png",
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        ],
+            // baris ke-2
+            Container(
+              margin: EdgeInsets.only(left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Jagonya",
+                    style: splashTextstyle2.copyWith(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Text(
+                    "Burger!",
+                    style: splashTextstyle2.copyWith(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    
+            
+            // baris ke-3
+            Container(
+              margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.06,
+              ),
+              child: Image.asset(
+                "assets/img-splash.png",
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
