7b772b03d8fea39796c11482f7f7086a55a27a98
M. Wahid Alqorni <alqorni@Ms-MacBook-Pro.local>
2024-05-18 13:27:15 +0700

    product detail page

---
+++ lib/pages/product_detail.dart
@@ -0,0 +1,136 @@
+import 'package:buburger_app/themes/themes.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetail extends StatefulWidget {
+  const ProductDetail({super.key});
+
+  @override
+  State<ProductDetail> createState() => _ProductDetailState();
+}
+
+class _ProductDetailState extends State<ProductDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Detail Burger",
+          style: blackTextstyle,
+        ),
+        backgroundColor: bgColor,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: blackColor,
+          ),
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(left: 20, right: 20),
+        child: ListView(
+          children: [
+            Image.asset(
+              "assets/burger1.png",
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Beef Burger",
+                      style: blackTextstyle,
+                    ),
+                    Text(
+                      "IDR 20.000",
+                      style: greyTextstyle,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      "assets/ic-kurang.png",
+                      width: 24,
+                      height: 24,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text("1"),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Image.asset(
+                      "assets/ic-tambah.png",
+                      width: 24,
+                      height: 24,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            // Detail
+
+            // Komposisi
+          ],
+        ),
+      ),
+      // BottomNavigation
+      bottomNavigationBar: Container(
+        width: double.infinity,
+        height: 80,
+        color: whiteColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // keranjang
+            Container(
+              width: 150,
+              height: 42,
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: primaryColor),
+                onPressed: null,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 5,
+                    bottom: 5,
+                    left: 19,
+                    right: 19,
+                  ),
+                  child: Text(
+                    "+ Keranjang",
+                    style: blackTextstyle,
+                  ),
+                ),
+              ),
+            ),
+
+            // Pesan Sekarang
+            Container(
+              width: 150,
+              height: 42,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: redColor, width: 2 ),
+              ),
+              child: Center(child: Text("Pesan Sekarang", style: secodaryTextstyle,)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/product_widget.dart
+++ lib/widgets/product_widget.dart
@@ -1,3 +1,4 @@
+import 'package:buburger_app/pages/product_detail.dart';
 import 'package:buburger_app/themes/themes.dart';
 import 'package:flutter/material.dart';
 
@@ -9,28 +10,33 @@ class ProductWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: whiteColor,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset(imageUrl, fit: BoxFit.cover, ),
-
-          Padding(
-            padding: const EdgeInsets.only(left: 13),
-            child: Text(nama, style: blackTextstyle.copyWith(
-              fontWeight: FontWeight.w600,
-            ), ),
-          ),
-
-           Padding(
-             padding: const EdgeInsets.only(left: 13),
-             child: Text(harga, style: greyTextstyle ),
-           ),
-        ],
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (builder) => ProductDetail() ));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: whiteColor,
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(imageUrl, fit: BoxFit.cover, ),
+    
+            Padding(
+              padding: const EdgeInsets.only(left: 13),
+              child: Text(nama, style: blackTextstyle.copyWith(
+                fontWeight: FontWeight.w600,
+              ), ),
+            ),
+    
+             Padding(
+               padding: const EdgeInsets.only(left: 13),
+               child: Text(harga, style: greyTextstyle ),
+             ),
+          ],
+        ),
       ),
     );
   }
