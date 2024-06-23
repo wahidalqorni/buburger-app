3be4c761653a7b094862ef64e0570190682cf250
M. Wahid Alqorni <alqorni@Ms-MacBook-Pro.local>
2024-06-23 11:09:32 +0700

    update config url web services

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,6 +2,38 @@ import 'package:intl/intl.dart';
 
 class Config {
 
+  // sediakan variabel penampung alamat url web service
+  static String urlAPI = "https://syntop.airportslab.com/api";
+  // sedaiakan variabel penampung alamat url main website (digunakan untuk ambil path file/gambar dr backend)
+  static String urlMain = "https://syntop.airportslab.com";
+
+  // variabel end point yg akan dipakai
+  // login, register, logout
+  String urlLogin = "$urlAPI/login";
+  String urlRegister = "$urlAPI/register";
+  String urlLogout = "$urlAPI/logout";
+
+  // product (burger)
+  String urlProdRekomen = "$urlAPI/product-rekomendasi";
+  String urlProdAll = "$urlAPI/product-list";
+  String urlProdNew = "$urlAPI/product-new";
+  String urlProdSearch = "$urlAPI/product-search?keyword=";
+
+  // keranjang
+  String urlKeranjangPost = "$urlAPI/keranjang-post";
+  String urlKeranjangList = "$urlAPI/keranjang-list";
+  String urlKeranjangDelete = "$urlAPI/keranjang-delete";
+
+  // checkout
+  String urlCheckoutPost = "$urlAPI/checkout-post";
+  String urlCheckoutPostById = "$urlAPI/checkout-post-by-id";
+  String urlCheckoutListBaru = "$urlAPI/checkout-list-baru?user_id=";
+  String urlCheckoutListProses = "$urlAPI/checkout-list-proses?user_id=";
+  String urlCheckoutListSelesai = "$urlAPI/checkout-list-selesai?user_id=";
+
+  // upload bukti bayar
+  String uploadBuktiBayar = "$urlAPI/upload-bukti-bayar";
+
   // membuat fungsi currency
   static String convertToIdr(dynamic nominal, int decimalDigit) {
     
